9fb2f8d8df7e38b20c8d08befd5aebe63757ce47
Charles Emrich and Ethan Luts <charles+ethan@>
2017-05-02 14:00:34 -0700

    add ability to list coping mechs and began refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
-var stressSymptoms = [],
-    healthSymptoms = [];
+var stressSymptoms   = [],
+    healthSymptoms   = [],
+    copingMechanisms = [];
 
 $(function() {
+//   function functionthatdoesitall (action) {
+//     action is either "add" or "remove"
+//   }
+//
+//   function itRemovesAThing()
+//   function itAddsAThing()
+
   $("form#stress-survey").change(function() {
     //Add currently checked selections to symptom arrays:
     $("input:checkbox[name='stress-symptoms']:checked").each(function() {
@@ -14,20 +22,31 @@ $(function() {
         healthSymptoms.push($(this).val());
       }
     });
+    $("input:checkbox[name='coping-mechanisms']:checked").each(function() {
+      if (copingMechanisms.indexOf($(this).val()) === -1) {
+        copingMechanisms.push($(this).val());
+      }
+    });
 
     //Remove currently unchecked selections from symptom arrays:
     $("input:checkbox[name='stress-symptoms']:not(:checked)").each(function() {
       if (stressSymptoms.indexOf($(this).val()) !== -1) {
         stressSymptoms.splice(stressSymptoms.indexOf($(this).val()), 1);
       }
-      });
+    });
       $("input:checkbox[name='health-symptoms']:not(:checked)").each(function() {
-        if (healthSymptoms.indexOf($(this).val()) !== -1) {
-          healthSymptoms.splice(healthSymptoms.indexOf($(this).val()), 1);
-        }
+      if (healthSymptoms.indexOf($(this).val()) !== -1) {
+        healthSymptoms.splice(healthSymptoms.indexOf($(this).val()), 1);
+      }
+    });
+      $("input:checkbox[name='coping-mechanisms']:not(:checked)").each(function() {
+      if (copingMechanisms.indexOf($(this).val()) !== -1) {
+        copingMechanisms.splice(copingMechanisms.indexOf($(this).val()), 1);
+      }
     });
 
-    console.log(healthSymptoms);
-    console.log(stressSymptoms);
+    //Show information depending on state of symptom arrays:
+
+    console.log([healthSymptoms, stressSymptoms, copingMechanisms]);
   });
 });
